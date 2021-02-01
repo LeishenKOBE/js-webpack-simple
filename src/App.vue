9ec4afc1e7cbc58c8a312ef484900caf1fe7164b
大家好我叫石同学 <wuyujiesong@outlook.com>
2021-02-02 00:06:31 +0800

    webpack+vue3

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>解析Vue文件了哟~</div>
+  <p>{{ name }}</p>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  setup() {
+    const name = ref('txm')
+
+    return {
+      name,
+    }
+  },
+})
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,126 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+const { VueLoaderPlugin } = require('vue-loader/dist/index')
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const notifier = require('node-notifier')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const webpack = require('webpack')
+
 module.exports = {
-  entry: "./src/index.js",
+  mode: 'development',
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    publicPath: "xuni",
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
   },
-  mode: "development",
-  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif|bmp)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+              },
+            },
+          },
+        },
+      },
+      {
+        test: /\.(mp4|ogg|mp3|wav)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 1024,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+              },
+            },
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      title: 'Vue3 + TS -> Web App',
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    new OptimizeCssAssetsWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new VueLoaderPlugin(),
+    new FriendlyErrorsWebpackPlugin({
+      onErrors: (severity, errors) => {
+        notifier.notify({
+          title: 'webpack 编译失败了~',
+          message: `${severity} ${errors[0].name}`,
+          subtitle: errors[0].file || '',
+        })
+      },
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disabled',
+      generateStatsFile: true,
+    }),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: false,
+    }),
+  ],
   devServer: {
     port: 3000,
     hot: true,
     open: true,
-    contentBase: "www"
+    contentBase: './dist',
   },
-};
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserWebpackPlugin()],
+  },
+  devtool: 'inline-source-map',
+}
